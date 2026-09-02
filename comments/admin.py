268195318bd2868218c9b40b3c87a9db0f26f1db
Vitaliy <paffos001@gmail.com>
2026-09-02 11:39:41 +0300

    Show avatar column in comment admin list

--- comments/admin.py
+++ comments/admin.py
@@ -3,7 +3,7 @@ from .models import Comment
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('user_name', 'email', 'created_at', 'parent', 'attachment_kind')
+    list_display = ('user_name', 'email', 'created_at', 'parent', 'attachment_kind', 'avatar')
     list_filter = ('created_at', 'parent')
     search_fields = ('user_name', 'email', 'text')
     ordering = ('-created_at',)
